abf59d9fce6e411ccaba53bbf70ff054ca6c894a
beazael <beazael@beazael-ubuntu.(none)>
2014-03-27 20:36:20 +0100

    test1

--- Tests/test1.ml
+++ Tests/test1.ml
@@ -1,8 +1,21 @@
 open Package'java'lang
 
-class point (this : (*Class encapsulée*) ) = 
+class point (this : java'lang'Object java_instance) = 
 object (self)
-  method this () = this;
+  method this_object () = this;
+  method displayP () = print_endline "point"
 end
 
 let p = new point (Java.make "java'lang'Object()" ())
+
+class pc (this : java'lang'String java_instance) =
+object (self)
+  inherit point (this:>java'lang'Object java_instance)
+  (*method  this_object () = this;*)
+  method display () = print_endline "pc"
+end
+
+let _ =
+  let p_c = new pc (Java.make "java'lang'String(java.lang.String)" (JavaString.of_string "blop")) in
+  p_c#display() ;
+  p_c#displayP()
