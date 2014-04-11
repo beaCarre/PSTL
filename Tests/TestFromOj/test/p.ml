c32746fd68b7e7c00939989543e13106da8b483a
CARRE BEATRICE <2900087@ari-31-207-12.ufr-info-p6.jussieu.fr>
2014-04-11 17:32:34 +0200

    test interf,extends etc

--- Tests/TestFromOj/test/p.ml
+++ Tests/TestFromOj/test/p.ml
@@ -1,22 +1,30 @@
 type top = java'lang'Object java_instance;;
 exception Null_object of string
 
-type _jni_jPoint = mypack'Point java_instance;;
+type _jni_jPoint = mypack'Point java_instance
+type _jni_jColored = mypack'Colored java_instance
 
 class type jPoint =
-  object
-    method _get_jni_jPoint : _jni_jPoint
-    method set_x : int32 -> unit
-    method get_x : unit -> int32
-    method set_y : int -> unit
-    method get_y : unit -> int
-    method moveto : int -> int -> unit
-    method rmoveto : int -> int -> unit
-    method toString : unit -> string
-    method display : unit -> unit
-    method distance : unit -> float
-    method eq : jPoint -> bool
-  end
+object
+  method _get_jni_jPoint : _jni_jPoint
+  method set_x : int -> unit
+  method get_x : unit -> int
+  method set_y : int -> unit
+  method get_y : unit -> int
+  method moveto : int -> int -> unit
+  method rmoveto : int -> int -> unit
+  method toString : unit -> string
+  method display : unit -> unit
+  method distance : unit -> float
+  method eq : jPoint -> bool
+end;;
+
+class type jColored =
+object
+  method _get_jni_jColored : _jni_jColored
+  method getColor : unit -> string
+  method setColor : string -> unit
+end
 
 class _capsule_jPoint =
   fun (jni_ref : _jni_jPoint) ->
@@ -29,7 +37,7 @@ object (self)
   method eq =
     fun (_p0 : jPoint) ->
       let _p0 = _p0#_get_jni_jPoint in
-      Java.call "mypack.Point.eq(mypack.Point):boolean" jni_ref _p0
+	Java.call "mypack.Point.eq(mypack.Point):boolean" jni_ref _p0
   method distance =
     fun () ->
       Java.call "mypack.Point.distance():double" jni_ref
@@ -65,12 +73,36 @@ object (self)
   method _get_jni_jPoint = jni_ref
 end;;
 
+class _capsule_jColored =
+  fun (jni_ref : _jni_jColored) ->
+    let _ =
+      if Java.is_null jni_ref
+      then raise (Null_object "mypack/Colored")
+      else ()
+    in
+object (self)
+  method getColor =
+    fun () ->
+      JavaString.to_string (Java.call "mypack.Colored.getColor():string" jni_ref)
+  method setColor =
+    fun _p0 ->
+      let _p0 = JavaString.of_string _p0 in
+	(Java.call "mypack.Colored.setColor(string)" jni_ref _p0)
+  method _get_jni_jColored = jni_ref
+end
+
 let jPoint_of_top (o : top) : jPoint =
   new _capsule_jPoint (Java.cast "mypack.Point" o);;
 
+let jColored_of_top (o : top) : jColored =
+  new _capsule_jColored (Java.cast "mypack.Colored" o)
+
 let _instance_of_jPoint (o : top) =
   Java.instanceof "mypack.Point" o;;
-  
+
+let _instance_of_jColored (o : top) =
+  Java.instanceof "mypack.Colored" o;;
+
 class point _p0 _p1 =
   let _p1 = Int32.of_int _p1 in
   let _p0 = Int32.of_int _p0 in
--- Tests/TestFromOj/test/p.mli
+++ Tests/TestFromOj/test/p.mli
@@ -1,25 +1,35 @@
 type top;;
 
 type _jni_jPoint;;
+type _jni_jColored;;
 
 class type jPoint =
-  object
-    method _get_jni_jPoint : _jni_jPoint
-    method set_x : int32 -> unit
-    method get_x : unit -> int32
-    method set_y : int -> unit
-    method get_y : unit -> int
-    method moveto : int -> int -> unit
-    method rmoveto : int -> int -> unit
-    method toString : unit -> string
-    method display : unit -> unit
-    method distance : unit -> float
-    method eq : jPoint -> bool
-  end;;
+object
+  method _get_jni_jPoint : _jni_jPoint
+  method set_x : int -> unit
+  method get_x : unit -> int
+  method set_y : int -> unit
+  method get_y : unit -> int
+  method moveto : int -> int -> unit
+  method rmoveto : int -> int -> unit
+  method toString : unit -> string
+  method display : unit -> unit
+  method distance : unit -> float
+  method eq : jPoint -> bool
+end;;
 
-val jPoint_of_top : top -> jPoint;;
+class type jColored =
+object
+  method _get_jni_jColored : _jni_jColored
+  method getColor : unit -> string
+  method setColor : string -> unit
+end;;
+
+val jPoint_of_top : top -> jPoint;; 
+val jColored_of_top : top -> jColored;;
 
 val _instance_of_jPoint : top -> bool;;
+val _instance_of_jColored : top -> bool;;
 
 class point : int -> int -> jPoint;;
 
